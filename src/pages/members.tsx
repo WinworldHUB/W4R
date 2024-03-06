763e0d5f6dcc10d85ef276207ea5959e5c42d709
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-03-06 23:05:10 +0530

    implement members api

--- src/pages/members.tsx
+++ src/pages/members.tsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, {useEffect} from "react";
 import PageLayout from "../lib/components/page-layout";
-import data from "../lib/data/users.json";
+// import data from "../lib/data/users.json";
 import MembersDataTable from "../lib/components/member/members-data-table";
+import useApi from "../lib/hooks/useApi";
 
 const Members = (pageProps: PageProps) => {
+  const { data, getData, postData } = useApi<Member[]>();
+  useEffect(() => {
+    getData("/members");
+  }, []);
+  const handleImportMembers= async(jsonData: Member[])=>{
+    try {
+      // Send the JSON data to the backend API using postData function
+      const responseData = await postData(
+        "/members",
+        jsonData
+      );
+      console.log("Response from server:", responseData);
+      getData("/members")
+  
+      // Handle response from the backend if needed
+    } catch (error) {
+      // Handle errors
+      console.error("Error:", error);
+    }
+  }
   return (
     <PageLayout {...pageProps}>
       <MembersDataTable
         data={data as Member[]}
         onDataImport={(data) => {
-          console.log(JSON.stringify(data));
+         handleImportMembers(data as Member[])
         }}
       />
     </PageLayout>
